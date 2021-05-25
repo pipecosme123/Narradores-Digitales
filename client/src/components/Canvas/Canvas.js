b14cc4bdfddc4a7972ad962da0aa42e2e2487a6c
Daniel Cosme <daniel.cosme@uao.edu.co>
2021-05-25 13:57:22 -0500

    Se cambio la ubicacion de la dependencia de 'p5' de la carpeta general a la carpeta 'client'

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+//import Avatar1 from './../img/hombre.svg';
 import P5 from 'p5';
+
+let img;
 
 class Canvas extends Component{
     constructor(props){
@@ -10,15 +13,27 @@ class Canvas extends Component{
     Sketch = (p) => {
         p.setup =() =>{
             p.createCanvas(500,500);
-        }
-        p.draw = () =>{
             p.background('grey');
+            img = p.loadImage('./../img/hombre.svg');
+        }
+        p.draw = () =>{           
+            p.image(img, 0, 0);
+            p.ellipse(150, 100, 100, 100);
+        }
+    }
+
+    Dibujar = (p) => {
+        p.draw = () =>{           
             p.ellipse(150, 100, 100, 100);
         }
     }
 
     componentDidMount(){
-        this.myp5 = new P5(this.Sketch, this.myRef.current)
+        this.myp5 = new P5(this.Sketch, this.myRef.current);
+        // let numero = prompt("numero");
+        // if(numero == 1){
+           // this.myp5 = new P5(this.Dibujar, this.myRef.current);
+        //}
     };
 
     render(){
